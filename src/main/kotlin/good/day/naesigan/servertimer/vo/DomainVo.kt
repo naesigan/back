3f4a0cdf605fd51873b0b71fc57aeef590700713
jinwook <77241161+happy-wook-kim@users.noreply.github.com>
2023-09-22 17:35:50 +0900

    [ADD] 도메인 조회 - id 값 노출 제거, prod 설정파일 추가

--- src/main/kotlin/good/day/naesigan/servertimer/vo/DomainVo.kt
+++ src/main/kotlin/good/day/naesigan/servertimer/vo/DomainVo.kt
@@ -3,7 +3,6 @@ package good.day.naesigan.servertimer.vo
 import java.sql.Timestamp
 
 data class DomainVo(
-    val id: Int,
     val name: String,
     val date: Timestamp?,
     val deleted: Timestamp?,
